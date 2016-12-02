c5e3eb1867b32bff3afa0a9a54eed606c9f7d78d
Irina117 <iraumetskaya@gmail.com>
2016-12-02 22:12:12 +0700

    Signed-off-by: Irina117 <iraumetskaya@gmail.com>

---
+++ Consruction/Authorization.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Consruction
+{
+    public partial class Authorization : Form
+    {
+        public Authorization()
+        {
+            InitializeComponent();
+        }
+
+        private void EnterInSystem_Click(object sender, EventArgs e)
+        {
+            if (LoginTB.Text == "moderator")
+            {
+                this.Hide();
+                var f1 = new formEmployers();
+                f1.Closed += (s, args) => this.Close();
+                f1.Show();
+            }
+
+            if (LoginTB.Text == "consultant")
+            {
+                this.Hide();
+                var f2 = new formEmployees();
+                f2.Closed += (s, args) => this.Close();
+                f2.Show();
+            }
+        }
+    }
+}
